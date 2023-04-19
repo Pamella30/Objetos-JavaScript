992336efb632fafb465d529af4f84e2a1b3c2d37
Pamella Cruz <suporte.pamella@gmail.com>
2023-04-19 11:10:45 -0300

    update

---
+++ exercicios/oo.js
@@ -0,0 +1,23 @@
+// codigo nao executavel
+
+//procedural
+processamento(valor1, valor2, valor3) //uma funcao que manipula os dados
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+} 
+
+objeto.processamento()// Foco passou a ser o objeto
+
+//4 Principios importantes da OO
+
+//Abstracao
+//encapsulamento
+//heranca
+//polimorfismo
